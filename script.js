536c2e915e7a08d0cf8c9bbbf47298d7f6948f49
Tyler Rutherford <rutherford87@gmail.com>
2021-03-31 15:39:32 -0500

    initial push to repo. Created index.html, script.js, and style.css. Intial blocking of two hourly rows completed. Psuedo coding functions in JS next

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+
+//set up function to establish what hour it is currently in the day
+//set up loop to set the class for each 'textarea' per row based on comparison to current time
+
+//build an array for number of rows (hours in a day) to show
+var hoursPerDay=[8, 9];
