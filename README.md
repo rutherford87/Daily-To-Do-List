60ad066564fcfd80b559e1cb858e06b35f679c77
Tyler Rutherford <rutherford87@gmail.com>
2021-04-01 21:41:59 -0500

    Added new example.PNG. Readme is up to date with user instructions. Script file includes loop for showing colors of past, present, and future hours using Moment. Next update will include storing items in local and getting.

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Daily-To-Do-List
+
+Contents of repo:
+* index.html
+* style.css
+* script.js
+* README.md
+* Example.PNG
+
+This is a daily to do list tool that tracks your to-dos by hour. 
+
+How to use this tool:
+* Each hour will be highlighted a different color:
+ * Grey means the hour has passed
+ * Red means you need to finish this task before the hour ends
+ * Green means you have time to work on those tasks
+* Type in to the form boxes and press the save button on the right to record each to-do
+
+The repo can be found here: 
+[TPR_GitHub](https://github.com/rutherford87/Daily-To-Do-List)
+
+The website can be viewed here: [Daily To-Do List](https://rutherford87.github.io/Daily-To-Do-List/)
+
+Here is a screenshot of the thrilling quiz:
+![WOW!](./Example.PNG)
+
+This was challenging but enjoyable. This tool was built with Bootstrap v4.5, JQuery, and Moment.
+
+Good luck, use this tool to get your stuff done!
--- script.js
+++ script.js
@@ -1,8 +1,53 @@
+//Build the elements:
+var textArea7AMEl = document.getElementById('7AM')
+var textArea8AMEl = document.getElementById('8AM')
+var textArea9AMEl = document.getElementById('9AM')
+var elementArray = [textArea7AMEl, textArea8AMEl, textArea9AMEl];
 
+//Use moment to establish current time and date
+var today = moment();
+console.log('today is from moment '+ today);
+var input = $(".input");
 
+$("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
 
+var currentHour = moment().format("HH");
+console.log (currentHour);
 //set up function to establish what hour it is currently in the day
 //set up loop to set the class for each 'textarea' per row based on comparison to current time
 
 //build an array for number of rows (hours in a day) to show
-var hoursPerDay=[8, 9];
+var hoursPerDay=[7, 8, 9];
+//var currentHour =7;
+//assign future to all rows intially?
+
+//if the hour assigned a row (how is it assigned --> doesn't need to be assigned, do it this way:
+currentHour=8;
+function timeCheck(){
+for (var i=0; i<=(hoursPerDay.length-1); i++) {
+if (hoursPerDay[i] < currentHour) {
+    elementArray[i].classList.add('past');
+} else if (hoursPerDay[i] == currentHour) {
+    elementArray[i].classList.add('present');
+} else {
+    elementArray[i].classList.add('future');
+}
+}
+console.log(elementArray);
+}
+
+//leave timeCheck here to run whenever the page is loaded. will it reload after a button click? 
+timeCheck();
+// might have to add in textArea7AMEl.classList.remove('past')
+//textArea7AMEl.classList.remove('future') in order to override pre-added classes
+
+
+//to revisit the class add function:
+// console.log('this is element array');
+// console.log(elementArray);
+// elementArray[1].classList.add('past');
+// console.log(elementArray);
+// textArea7AMEl.classList.add('present');
+// console.log(elementArray);
+// textArea7AMEl.classList.remove('present');
+// console.log(elementArray);
